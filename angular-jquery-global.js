75d2e9872d44e8dd7b1f83d2744aa6fd07b7d1ec
danicomas <danicomas@gmail.com>
2014-09-04 12:51:58 +0200

    Added JavaScripts

---
+++ angular-jquery-global.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+angular.module('angular-jquery-global', []).factory('jqueryGlobal', [function () {
+    return $.global;
+}]).provider('$jqueryGlobal', function () {
+    return {
+        preferCulture: function (language) {
+            $.global.preferCulture(language);
+        },
+        $get: function () {
+            return {
+
+            }
+        }
+    }
+});
